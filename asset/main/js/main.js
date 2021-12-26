c3e9fa465bfb30ba032ac93bf48b58bb0c5215c2
hanz <rayhanzamzamy.portable@gmail.com>
2021-12-26 11:21:44 +0700

    2nd

--- asset/main/js/main.js
+++ asset/main/js/main.js
@@ -1,38 +1,70 @@
+let audVzr = new Wave();
+let audDat = [{'title': 'Un Monde Sans Danger (Instrumental)', 'source': 'asset/audio/un-monde-sans-danger.mp3'}];
+let audIdx = 0;
+let cvsSiz = Math.max($(window).height(), $(window).width());
+let cvsDOM = document.getElementById('wavevw');
 let audDOM = document.getElementById('audsrc');
-let plyBtn = $('#pl');
-let pseBtn = $('#ps');
 
-$(document).ready(function () {
-    pseBtn.hide();
+cvsDOM.height = cvsSiz;
+cvsDOM.width = cvsSiz;
+audVzr.fromElement('audsrc', 'wavevw', {type: 'flower', colors:['#000000']});
+audLod(audIdx);
+audUpt();
 
-    let audVzr = new Wave();
-    let player = $('#player');
-    let cvsSiz = Math.max(player.height(), player.width());
-    let cvsDOM = document.getElementById('wavevw');
-    cvsDOM.height = cvsSiz;
-    cvsDOM.width = cvsSiz;
-    audVzr.fromElement('audsrc', 'wavevw', {type: 'flower', colors:['#ababab']});
-})
+function sec2Timestamp(sec) {
+    let hr = Math.floor(sec / 3600);
+    let mn = Math.floor((sec - (hr * 3600)) / 60);
+    let sc = Math.floor(sec - (hr * 3600) - (mn * 60));
 
-function play() {
-    audDOM.load();
+    hr = (hr < 10) ? '0' + hr : hr;
+    mn = (mn < 10) ? '0' + mn : mn;
+    sc = (sc < 10) ? '0' + sc : sc;
+
+    return hr + ':' + mn + ':' + sc;
+}
+
+function audTrg(){
+    $('#pl').toggleClass('d-none');
+    $('#ps').toggleClass('d-none');
+}
+
+function audUpt(){
+    $('#audtime').html(sec2Timestamp(audDOM.currentTime));
+}
+
+function audLod(idx){
+    audDOM.src = audDat[idx].source;
+    $('#audtitle').html(audDat[idx].title);
+}
+
+function btnPly() {
     audDOM.play();
-    plyBtn.hide();
-    pseBtn.show();
 }
 
-function pause() {
+function btnPse() {
     audDOM.pause();
-    pseBtn.hide();
-    plyBtn.show();
 }
 
-function backward() {
-    pause();
-    audDOM.currentTime = 0;
+function btnBwd() {
+    btnPse();
+    if (audDOM.currentTime !== 0){
+        audDOM.currentTime = 0;
+    }else{
+        audIdx = (audIdx > 0) ? audIdx - 1 : audIdx;
+        audLod(audIdx);
+    }
+    audUpt();
 }
 
-function forward() {
-    pause();
-    audDOM.currentTime = audDOM.duration;
+function btnFwd() {
+    btnPse();
+
+    btnPse();
+    if (audDOM.currentTime !== audDOM.duration){
+        audDOM.currentTime = audDOM.duration;
+    }else{
+        audIdx = (audIdx < audDat.length) ? audIdx + 1 : audIdx;
+        audLod(audIdx);
+    }
+    audUpt();
 }
